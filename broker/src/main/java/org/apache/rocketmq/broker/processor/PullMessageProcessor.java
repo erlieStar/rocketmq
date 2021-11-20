35db0e2128c723320b95fcc9cac1bf6c80b67881
lilimin <290059742@qq.com>
2021-11-20 17:24:26 +0800

    消费失败重试过程

--- broker/src/main/java/org/apache/rocketmq/broker/processor/PullMessageProcessor.java
+++ broker/src/main/java/org/apache/rocketmq/broker/processor/PullMessageProcessor.java
@@ -69,6 +69,9 @@ import org.apache.rocketmq.store.PutMessageResult;
 import org.apache.rocketmq.store.config.BrokerRole;
 import org.apache.rocketmq.store.stats.BrokerStatsManager;
 
+/**
+ * 处理消息拉取
+ */
 public class PullMessageProcessor extends AsyncNettyRequestProcessor implements NettyRequestProcessor {
     private static final InternalLogger log = InternalLoggerFactory.getLogger(LoggerName.BROKER_LOGGER_NAME);
     private final BrokerController brokerController;
